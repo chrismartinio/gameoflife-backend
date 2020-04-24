45f0719392e1c508ebded8ecaf690ccb6f74bec4
ChrisMartin-io <chris@sensedev.com>
2020-04-24 13:29:02 -0700

    bug fixes

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get("/dogs/:name", function(req, res) {
 
 /** Start server on port 3000 */
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000.");
 });
 
