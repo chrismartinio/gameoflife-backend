bdc91e70c4fd61aad1d389a31b059b4065ac28bb
Chris Martin <chrismartin@XM9PYLQ47D.it.kandji.corp>
2023-06-25 18:14:39 -0700

    cleanup / lint
    3
    
    4
    
    5

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
 app.listen(3002, function () {
-  console.log("Game of Life Demo - listening on 3002?!");
+  console.log("Game of Life Demo - listening on 3002?!!!");
 });
